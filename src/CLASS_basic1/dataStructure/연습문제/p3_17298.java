7c2620c9cbf8ff137765b503193826f14fb0b0da
user <user@user-PC>
2020-02-25 18:04:56 +0900

    17298 몬가 시도는 해봤으나 해결이 안남..

--- src/CLASS_basic1/dataStructure/연습문제/p3_17298.java
+++ src/CLASS_basic1/dataStructure/연습문제/p3_17298.java
@@ -5,8 +5,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 /**
 @문제
@@ -42,21 +43,70 @@ import java.util.StringTokenizer;
 	책에서는 그랬는데 여기선 걍 값을 넣는 듯 함.
 	
 	아 근데 분석을 했는데 .. 9으로 시작하는 수열은 nge가 -1인거 보면 인덱스마다 한번씩 다 도는 것 같은데..
-	몰겠다 주말에 다시 하기ㅠ
+	0225
+	오큰수의 정의를 제대로 알고 싶어서 찾아봤는데 "오큰수 = Ai보다 큰 수 중에 가장 가까운 수" 란다..다시 해봅니다..
+	처음에 nge를 스택이 아니라 int형 배열로 했었는데 그래서 beforeIdx랑 beforeNum를 따로 뒀었는데 stack으로 하면서 바꿈..
+	그게 사실 해당 수열에의 인덱스 그대로 nge 인덱스에 저장하게 하는거라 해서 첨에 int형 배열로 할라 했던거임.
+	돌려보자.. 이 인덱스에 맞춰서 안해도 되는건지.
+	근데 애초에 다른 예제로 하니까 ㅋㅋㅋㅋㅋㅋㅋ틀림 하
+	4
+	9 5 4 8
+	이 경우일 때는 안돼. -1 8 8 -1 이게 안나온다.
+	대가리 안돌아가서 못풀겠다 효율 개안남 5시간동안 이것만 보고 있었는 듯ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+	
 */
 public class p3_17298 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
 		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter(System.out) );
+		/*
+		 4
+		3 5 2 7
+		-> 5 7 7 -1
 		
-		int				cnt = Integer.parseInt( br.readLine() );
-		int[] 			seq = Integer.parseInt(br.readLine().split(" "));
+		4
+		9 5 4 8
+		-> -1 8 8 -1
+		 */
+		int				cnt = Integer.parseInt( br.readLine() ); //총 수
+		String[] 		seq = br.readLine().split(" "); // 문자열..
 		Stack<Integer> stack= new Stack<Integer>();
-		Stack<Integer> nge	= new Stack<Integer>();
+		Queue<Integer>	nge	= new LinkedList<Integer>();
+		int 			i = 0;
 		
-		int size = seq.countTokens();
-		int	big = 0;
+		while( nge.size()!=cnt ) {
+			int currentNum = Integer.parseInt( seq[i] ); // 수열의 기준 수 - 3, 5, 2, 7
+			if( i==0 ) {
+				stack.push(i++); // 0
+			}else {
+				int beforeNum = Integer.parseInt( seq[stack.peek()] );  // 그 인덱스에 해당되는 실제 수 - 3, 5, 2 
+				if( beforeNum < currentNum ) { // 3<5 , 5>2, 2<7
+					stack.pop();
+					nge.offer( currentNum ); // 5
+					//nge[beforeIdx] = currentNum;  //nge[0]=5, num[2]=7,
+					if( stack.isEmpty() ) {
+						if( i==cnt-1 ) {
+							nge.offer( -1 );
+						}else {
+							stack.push( i ); // 스택 - 1
+						}
+					}
+				}else {
+					stack.push( i ); // 2.. 그럼 스택에는 지금까지 1,2가 있음
+				}
+				
+				if( i!=cnt-1) {
+					i++;
+				}
+			}
+		}
 		
+		while( !nge.isEmpty() ) {
+			bw.write( String.valueOf(nge.poll())+" " );
+		}
+		
+		
+		/*
 		while( cnt-->0 ) {
 			int currentNum = Integer.parseInt( seq.nextToken() ); //수열의 기준 수
 			if( cnt+1==size ){
@@ -75,7 +125,7 @@ public class p3_17298 {
 				}
 			}
 		}
-		
+		*/
 		
 		
 		br.close();
