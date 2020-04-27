9870980e24eba9707ba5d75f05ac773ce9a4a927
dada <dada@I-LUV-DALONG>
2020-04-28 02:04:42 +0900

    - 문제 거의 다 풀었는데 마무리 하기.

--- src/CLASS_Algorithm_Basic/Graph/연습문제/_2178.java
+++ src/CLASS_Algorithm_Basic/Graph/연습문제/_2178.java
@@ -5,16 +5,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 
 	@Question
 		N×M크기의 배열로 표현되는 미로가 있다.
-		1	0	1	1	1	1
+	여기1	0	1	1	1	1
 		1	0	1	0	1	0
 		1	0	1	0	1	1
-		1	1	1	0	1	1
+		1	1	1	0	1	1까지
 		미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다.
 		이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오.
 		한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
@@ -62,15 +63,80 @@ import java.util.List;
 		
  */
 public class _2178 {
+	
+	private static int[] dx = {-1,1,0,0}, dy = {0,0,1,-1}; // 좌우상하
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
-		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter(System.out) );
+		BufferedReader	br			= new BufferedReader( new InputStreamReader(System.in) );
+		BufferedWriter	bw			= new BufferedWriter( new OutputStreamWriter(System.out) );
+	
+		String[]		raw			= br.readLine().split(" ");
+		int				N			= Integer.parseInt( raw[0] ), M = Integer.parseInt( raw[1] );
+		int[][]			mazeMatrix	= new int[N][M];	// 세로,가로
+		int[][]			groupMatrix	= new int[N][M];	// 세로,가로
+		int minCnt = 0; // 최소한의 칸수
 		
-		int cnt = br.readLine();
-		int[][] mazeMatrix = 
+		// 값 초기화
+		for( int i=0;i<N;i++ ){
+			String num = br.readLine();
+			for( int j=0;j<M;j++ ){
+				if( Integer.parseInt(String.valueOf(num.charAt(j)))!=0 ){
+					mazeMatrix[i][j] = 1;
+				}
+			}
+		}
+		
+		// 인접한 칸들의 거리 측정하기.. TODO 측정이 제대로 안돼,,ㅠㅠ 17 이따구로 나옴...먼가 필터가 필요할 듯..
+		for( int i=0;i<N;i++ ){
+			for( int j=0;j<M;j++ ){
+				if( mazeMatrix[i][j]==1 && groupMatrix[i][j]==0 ){
+					BFS( mazeMatrix, groupMatrix, i, j, ++minCnt, N, M );
+				}
+			}
+		}
+		
+		// 인접한 칸들의 최단거리 구하기
+		
 		
 		br.close();
 		bw.flush();
 		bw.close();
+	}
+	
+	public static void BFS( int[][] mazeMatrix, int[][] groupMatrix, int i, int j, int minCnt, int N, int M ){
+		Queue<COORDINATE> queue = new LinkedList<COORDINATE>();
+		/*	생각은 하고 알고리즘을 푸는 것일까..?
+						그룹번호를 매길 필요 없기 떄문에 단순히 체크용도로만 쓰기 위한 값들만 있음 되는데
+						그저 앞에 문제에서 그렇게 했다고 하는 것 하고는... 에휴..ㅎㅎ...
+						TODO 내일 수정하기. 정답 따로.. 내가 한 것 따로
+		 */
+		queue.add( new COORDINATE(i, j) );
+		groupMatrix[i][j] = minCnt; // 거리 측정을 위해 현 위치로부터 4방위에 있는 것들은 +1을 해줘야함. 현 위치에서 거리 +_1로 위치하는 거니까.
+		
+		while( !queue.isEmpty() ){
+			COORDINATE c = queue.remove();
+			i = c.x;
+			j = c.y;
+			for( int k=0;k<4;k++ ){
+				int realX = i+dx[k], realY =j+dy[k];
+				if( realX>=0 && realX<N && realY>=0 && realY<M ){
+					/*
+					 * 뭔가 거리를 측정해야할 것 같은데..현재 위치에서 1인 거리일 때만 움직일 수 있게..0이 아니면 무조건 들어가게..
+					 * realX,realY로 잡아어야 했는데 i,j로 잡고 했었다..그래서 그런지
+						[1, 0, 2, 3, 4, 5], 
+						[6, 0, 7, 0, 8, 0], 
+						[9, 0, 10, 0, 11, 12],
+						[13, 14, 15, 0, 16, 17]
+						가나옴..
+						바꿔넣으니 15까지만 나온다.
+					 */
+					if( mazeMatrix[realX][realY]==1 && groupMatrix[realX][realY]==0 ){
+						queue.add( new COORDINATE(realX, realY) );
+						groupMatrix[realX][realY] = groupMatrix[i][j]+1;
+					}
+				}
+			}
+		}
+		
 	}
 }
