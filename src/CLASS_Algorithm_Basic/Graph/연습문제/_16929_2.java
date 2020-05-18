cc635c5532671b82e09e284b01b487b317022f52
user <user@user-PC>
2020-05-18 18:11:24 +0900

    - 도전 중..

--- src/CLASS_Algorithm_Basic/Graph/연습문제/_16929_2.java
+++ src/CLASS_Algorithm_Basic/Graph/연습문제/_16929_2.java
@@ -77,11 +77,15 @@ import java.util.StringTokenizer;
 		
 		왜자꾸 틀리는지 이해가 안갔는데 접근 자체가 잘못된 듯..
 		재귀로 풀어야 하는 듯 하다..
+		=> 재귀로 푼 것
+		
+		재귀로 바꿔야함..
 */
-public class _16929 {
+public class _16929_2 {
 	private static int[] dx = { -1,1,0,0 },
 						 dy = { 0,0,1,-1 };
 	private static boolean[][]	checkMatrix; // 방문배열
+	private static char[][]	BOARD; // 방문배열
 	
 	public static void main(String args[]) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -90,33 +94,28 @@ public class _16929 {
 		StringTokenizer st= new StringTokenizer( br.readLine() );
 		int				N = Integer.parseInt( st.nextToken() ),
 						M = Integer.parseInt( st.nextToken() );
-		Set<Character>	numSet	= new HashSet<Character>();
-		char[][]	BOARD		= new char[N][M];					// 세로가로
+		BOARD		= new char[N][M];					// 세로가로
 		checkMatrix	= new boolean[N][M];
 		
 		for( int i=0;i<N;i++ ){
 			String line = br.readLine();
 			for( int j=0;j<M;j++ ){
-				char each	= line.charAt(j);
-				BOARD[i][j] = each;
-				numSet.add(each);
+				BOARD[i][j] = line.charAt(j);
 			}
 		}
 		
-		for( Character each : numSet ) {
-			int value = 0;
-			// 이렇게 되면 매번 다 돌고나서 확인을 하니까 시간이 많이 걸리게 돼..
-			for( int i=0;i<N;i++ ){
-				for( int j=0;j<M;j++ ){
-					value += BFS( BOARD, each, N, M, i, j );
+		for( int i=0;i<N;i++ ){
+			for( int j=0;j<M;j++ ){
+				if( !checkMatrix[i][j] ){
+					if( BFS( BOARD[i][j], N, M, i, j ) ){
+						bw.write("Yes\n");
+						bw.flush();
+						System.exit(0);
+					}
 				}
 			}
-			if( value>=4 && value%2==0 ){
-				bw.write("Yes\n");
-				bw.flush();
-				System.exit(0);
-			}
 		}
+		
 		bw.write("No\n");
 		
 		br.close();
@@ -124,46 +123,42 @@ public class _16929 {
 		bw.close();
 	}
 
-	private static int BFS(char[][] BOARD, char each, int N, int M, int i, int j ){
-		Queue<POSITION>	queue		= new LinkedList<POSITION>();
-		//boolean[][]		checkMatrix	= new boolean[N][M]; // 방문배열 // 이것도 마찬가지로 안에서 체크해줘야한다고 생각했는데 밖에 위치하면서 전체적인 방문 횟수를 기록해줘야함.
-		int cnt=0;
-		
+	//private static int BFS(char[][] BOARD, char each, int N, int M, int i, int j ){
+	private static boolean BFS( char each, int N, int M, int i, int j ){
 		/*
-		// 이걸 왜해준거지..? 몬 정신이지..ㅋㅋㅋㅋㅋㅋㅋ
-		// 뭔가 첫번쨰 인덱스에서는 아무것도 없고
-		// 나와야하는 결과값에서 -1 되어져서 나와서 첫번쨰 방문하는 곳에는 체크를 해야한다고 한 것 같은데
-		// 그건 애초에 호출되는 부분부터가 틀렸던거라 의미없다. 
-		if( i==0 && j==0 ) {
-			checkMatrix[0][0] = true;
-		}
-		*/
+		Queue<POSITION>	queue	= new LinkedList<POSITION>();
+		int				cnt		= 0;
 		
 		queue.add( new POSITION(i, j) );
-		
-		// 이렇게 BFS 함수 안에서 each를 하나씩 접근하려고 했는데 메인함수에서 호출하면서 바꿔가면서 해줘야하는 걸로 바꿈
-		//for( int i=0;i<numList.size();i++ ) {
-			//char		each		= numList.get(i);
+		while( !queue.isEmpty() ){
+			POSITION P = queue.poll();
+			int curX = P.x,
+				curY = P.y;
+		 */
+			if( checkMatrix[i][j] ){
+				return true;
+			}
 			
-			while( !queue.isEmpty() ){
-				POSITION P = queue.poll();
-				int curX = P.x,
-					curY = P.y;
-				
-				for( int k=0;k<4;k++ ){
-					int realX = dx[k]+curX,
-						realY = dy[k]+curY;
-					if( realX>=0 && realX<N && realY>=0 && realY<M ) {
-						if( each==BOARD[realX][realY] && !checkMatrix[realX][realY] ){
-								cnt++;
-								checkMatrix[realX][realY] = true;
-								queue.add( new POSITION(realX, realY) );
-						}
+			checkMatrix[i][j] = true;
+			
+			for( int k=0;k<4;k++ ){
+				int realX = dx[k]+i,
+					realY = dy[k]+j;
+				if( realX>=0 && realX<N && realY>=0 && realY<M ) {
+					if( each==BOARD[realX][realY] && !checkMatrix[realX][realY] ){
+						/*
+						cnt++;
+						checkMatrix[realX][realY] = true;
+						queue.add( new POSITION(realX, realY) );
+						*/
+						if( BFS( each,N,M,realX,realY,) )
 					}
 				}
 			}
-		//}
-		return cnt;
+		/*
+		}
+		*/
+		return false;
 	}
 	
 }
