107739fe862aa2c1ec35907d3ead0ebbd2f60174
YunDaHyee <yon914@naver.com>
2021-02-08 17:49:19 +0900

    1강 문제 완

--- src/PROBLEM_stepBystep/_1/_10952.java
+++ src/PROBLEM_stepBystep/_1/_10952.java
@@ -16,11 +16,13 @@ import java.util.StringTokenizer;
 	@Input
 		입력은 여러 개의 테스트 케이스로 이루어져 있다.
 		각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		입력의 마지막에는 0 두 개가 들어온다.
 		1 1
 		2 3
 		3 4
 		9 8
 		5 2
+		0 0
 	@Output
 		각 테스트 케이스마다 A+B를 출력한다.
 		2
@@ -29,22 +31,25 @@ import java.util.StringTokenizer;
 		17
 		7
 	@history
-		1년 전에는 문자열로 받은 뒤에 split으로 잘라서 쓰는 걸로 했고 시간초가 훨씬 적게 걸렸던데
-		왜 똑같은 소스로 했는데도 지금은 더 걸리는거지..?
-		자바 버전 8->11 인 차이밖에 없는데 그 차이가 큰가,,
+		1년 전에는 반복문 들어가기 전에 1회차 입력을 먼저 받고 반복문 안에서는 마지막 쯤에 입력을 받는 거루 했다.
+		그렇게 할까 하다가 두번의 입력문이 들어가는 게 중복되는 느낌이어서 퐘하는 거로다가 했는데 흠.. 뭐가 더 효율적일까
+		근데 그렇게 시간차가 크게 날 것 같진 않으니까 중복이 되지 않는 거로 가야겠다.
  *		
  */
-public class _10951 {
+public class _10952 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader	br	= new BufferedReader( new InputStreamReader(System.in) );
 		BufferedWriter	bw	= new BufferedWriter( new OutputStreamWriter(System.out) );
 		StringTokenizer st	= null;
-		String			raw = "";
 		
-		while( (raw = br.readLine())!=null ){
-			st = new StringTokenizer(raw);
-			int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-			bw.write( String.valueOf(sum)+"\n" );
+		while(true){
+			st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			if( num1==0 && num2==0 ) {
+				break;
+			}
+			bw.write( String.valueOf(num1 + num2)+"\n" );
 		}
 		
 		bw.flush();
--- src/PROBLEM_stepBystep/_1/_11021.java
+++ src/PROBLEM_stepBystep/_1/_11021.java
@@ -14,37 +14,38 @@ import java.util.StringTokenizer;
 	@Question
 		두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 	@Input
-		입력은 여러 개의 테스트 케이스로 이루어져 있다.
+		첫째 줄에 테스트 케이스의 개수 T가 주어진다.
 		각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		5
 		1 1
 		2 3
 		3 4
 		9 8
 		5 2
 	@Output
-		각 테스트 케이스마다 A+B를 출력한다.
-		2
-		5
-		7
-		17
-		7
+		각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+		Case #1: 2
+		Case #2: 5
+		Case #3: 7
+		Case #4: 17
+		Case #5: 7
 	@history
-		1년 전에는 문자열로 받은 뒤에 split으로 잘라서 쓰는 걸로 했고 시간초가 훨씬 적게 걸렸던데
-		왜 똑같은 소스로 했는데도 지금은 더 걸리는거지..?
-		자바 버전 8->11 인 차이밖에 없는데 그 차이가 큰가,,
  *		
  */
-public class _10951 {
+public class _11021 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader	br	= new BufferedReader( new InputStreamReader(System.in) );
 		BufferedWriter	bw	= new BufferedWriter( new OutputStreamWriter(System.out) );
 		StringTokenizer st	= null;
-		String			raw = "";
+		int				cnt = 0;
+		int				i	= 0;
+		cnt = Integer.parseInt( br.readLine() );
 		
-		while( (raw = br.readLine())!=null ){
-			st = new StringTokenizer(raw);
-			int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-			bw.write( String.valueOf(sum)+"\n" );
+		while(i++<cnt){
+			st = new StringTokenizer( br.readLine() );
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			bw.write( "Case #"+i+": "+String.valueOf(num1 + num2)+"\n" );
 		}
 		
 		bw.flush();
