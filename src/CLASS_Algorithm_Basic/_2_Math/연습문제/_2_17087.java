57512da28bc925c03485ab0fda5b4d354233cfb5
YunDaHyee <yon914@naver.com>
2021-11-25 03:38:40 +0900

    Math/연습문제-2,6번 완

--- src/CLASS_Algorithm_Basic/_2_Math/연습문제/_2_17087.java
+++ src/CLASS_Algorithm_Basic/_2_Math/연습문제/_2_17087.java
@@ -13,21 +13,74 @@ import java.util.StringTokenizer;
 
 /**
 	@문제
-		
+		수빈이는 동생 N명과 숨바꼭질을 하고 있다.		
+		수빈이는 현재 점 S에 있고, 동생은 A1, A2, ..., AN에 있다.
+		수빈이는 걸어서 이동을 할 수 있다.		
+		수빈이의 위치가 X일때 걷는다면 1초 후에 X+D나 X-D로 이동할 수 있다.		
+		수빈이의 위치가 동생이 있는 위치와 같으면, 동생을 찾았다고 한다.
+		모든 동생을 찾기위해 D의 값을 정하려고 한다.		
+		가능한 D의 최댓값을 구해보자.
 	@입력
-	
+		첫째 줄에 N(1 ≤ N ≤ 105)과 S(1 ≤ S ≤ 109)가 주어진다.		
+		둘째 줄에 동생의 위치 Ai(1 ≤ Ai ≤ 109)가 주어진다.		
+		동생의 위치는 모두 다르며, 수빈이의 위치와 같지 않다.
+		1.
+			3 3
+			1 7 11
+		2.
+			3 81
+			33 105 57
+		3.
+			1 1
+			1000000000
 	@출력
-		
+		가능한 D값의 최댓값을 출력한다.
+		1. 2
+		2. 24
+		3. 999999999
 	@HISTORY
+		모든 동생을 찾기 위한 D의 최대값 즉, 공통된 최소한의 D 중 최대값 구하기 => 최대공약수 문제
+		현재 위치와 각각 동생들과의 거리차 D들을 구한다.
+		그리고 그 중에 최대공약수 구하기
+		
+	@Date
+		2021. 11. 24.
  */
 
 public class _2_17087 {
 	public static void main(String args[]) throws IOException {
-		BufferedReader	br			= new BufferedReader( new InputStreamReader(System.in) );
-		BufferedWriter	bw			= new BufferedWriter( new OutputStreamWriter(System.out) );
-	
-		bw.flush();
-		bw.close();
+		BufferedReader	br	= new BufferedReader( new InputStreamReader(System.in) );
+        
+		String[] NS = br.readLine().split(" ");
+		String[] Ai = br.readLine().split(" ");
+		
+		int		headCount	= Integer.parseInt(NS[0]);
+		int 	currentIndex= Integer.parseInt(NS[1]);
+		int[]	Ds			= new int[headCount];
+		int		result		= 0;
+		
+		for( int i=0;i<headCount;i++ ){
+			Ds[i]=Math.abs(currentIndex-Integer.parseInt(Ai[i]));
+		}
+		
+		result = Ds[0];
+		
+		for( int i=1;i<headCount;i++ ){
+			result = getGCD(result,Ds[i]);
+		}
+		
+		System.out.println(result);
+		
 		br.close();
+	}
+
+	public static int getGCD(int numberA, int numberB) {
+		while( numberB!=0 ){
+			int nmg = numberA%numberB;
+			numberA = numberB;
+			numberB = nmg;
+		}
+		
+		return numberA;
 	}
 }
--- src/CLASS_Algorithm_Basic/_2_Math/연습문제/_6_17103.java
+++ src/CLASS_Algorithm_Basic/_2_Math/연습문제/_6_17103.java
@@ -13,21 +13,70 @@ import java.util.StringTokenizer;
 
 /**
 	@문제
+		골드바흐의 추측: 2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다.
+		짝수 N을 두 소수의 합으로 나타내는 표현을 골드바흐 파티션이라고 한다.
+		짝수 N이 주어졌을 때, 골드바흐 파티션의 개수를 구해보자.
+		두 소수의 순서만 다른 것은 같은 파티션이다.
 		
 	@입력
-	
+		첫째 줄에 테스트 케이스의 개수 T (1 ≤ T ≤ 100)가 주어진다.
+		각 테스트 케이스는 한 줄로 이루어져 있고, 정수 N은 짝수이고, 2 < N ≤ 1,000,000을 만족한다.
+		5
+		6
+		8
+		10
+		12
+		100	
 	@출력
-		
+		각각의 테스트 케이스마다 골드바흐 파티션의 수를 출력한다.
+		1
+		1
+		2
+		1
+		6
 	@HISTORY
+		골든바흐의 추측
+		짝 = 소1+소2
+		소수 == 2 이상의 수, 루트N보다 작같 자연수로 떨어지면 안된다.
+		2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다 => 5보다 큰 홀수는 세 소수의 합으로 표현 가능하다.
+		N = a+b => N-b = a
+		
+	@Date
+		2021. 11. 24. 
  */
 
 public class _6_17103 {
 	public static void main(String args[]) throws IOException {
-		BufferedReader	br			= new BufferedReader( new InputStreamReader(System.in) );
-		BufferedWriter	bw			= new BufferedWriter( new OutputStreamWriter(System.out) );
-	
-		bw.flush();
-		bw.close();
+		BufferedReader	br		= new BufferedReader( new InputStreamReader(System.in) );
+		StringBuilder 	result	= new StringBuilder();
+		
+		int		count	= Integer.parseInt(br.readLine());
+		int		even	= 1000000;
+		boolean[] flags = new boolean[even];
+		
+		flags[0] = flags[1] = true;
+		
+		for( int i=2;i<even;i++ ){
+			if( !flags[i] ){
+				for( int j=i*2;j<even;j+=i ){ // 두번째 배수부터니까
+					flags[j] = true;
+				}
+			}
+		}
+		
+		while( count-->0 ){
+			int partition = 0;
+			even = Integer.parseInt(br.readLine());
+			for( int i=2;i<=even/2;i++ ){ // 소수가 2부터니까
+				if(!flags[i]&&!flags[even-i]){
+					partition++;
+				}
+			}
+			result.append(partition+"\n");
+		}
+		
+		System.out.println(result);
+		
 		br.close();
 	}
 }
