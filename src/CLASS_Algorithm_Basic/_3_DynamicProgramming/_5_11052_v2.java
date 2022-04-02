ecc7acd9689ef355deda894494b0ed9e063360e9
YunDaHyee <yon914@naver.com>
2022-04-02 23:00:05 +0900

    변수명 변경

--- src/CLASS_Algorithm_Basic/_3_DynamicProgramming/_5_11052_v2.java
+++ src/CLASS_Algorithm_Basic/_3_DynamicProgramming/_5_11052_v2.java
@@ -96,19 +96,19 @@ import java.io.InputStreamReader;
 				i<=N : 경우에 따라서는 한번에 n개 들어있는 카드팩을 구매할 수 있음
 	
 		
-		for( int i=1;i<cardPackPrice.length;i++ ){
+		for( int i=1;i<cardPackPrices.length;i++ ){
 			memo[i] = TopDown(n-i);
-			memo[n] = Math.max( TopDown(n), memo[i]+cardPackPrice[i] );
+			memo[n] = Math.max( TopDown(n), memo[i]+cardPackPrices[i] );
 		}
 		
 		이렇게 하면 n-i는 구할 수 있지만 n은 영원히 구할 수 없는데 어떻게 해야되지..?
 		=> 대안 : 
-				for( int i=1;i<cardPackPrice.length;i++ ){
+				for( int i=1;i<cardPackPrices.length;i++ ){
 					if( n-i <= 0 ){// 계쏙 뺴주는 거라서 전처리 필요
 						break;
 					}
 					memo[i] = TopDown(n-i); 
-					memo[n] = Math.max( memo[n], memo[i]+cardPackPrice[i] );
+					memo[n] = Math.max( memo[n], memo[i]+cardPackPrices[i] );
 				}
 				이렇게 하긴 했지만 답이 일치하지가 않다..
 		=> 대안 :
@@ -116,11 +116,11 @@ import java.io.InputStreamReader;
 			max를 그떄마다 줘서 그 값과 비교하게 해야한다.
 			int max = 0;
 		
-			for( int i=1;i<cardPackPrice.length;i++ ){
+			for( int i=1;i<cardPackPrices.length;i++ ){
 				if( n-i <= 0 ){// 계쏙 뺴주는 거라서 전처리 필요
 					break;
 				}
-				max = Math.max( max, TopDown(n-i)+cardPackPrice[i] );
+				max = Math.max( max, TopDown(n-i)+cardPackPrices[i] );
 			}
 			
 				
@@ -129,7 +129,7 @@ import java.io.InputStreamReader;
  */
 
 public class _5_11052_v2 {
-	private static int[] memo = new int[1001], cardPackPrice=new int[1001];
+	private static int[] memo = new int[1001], cardPackPrices=new int[1001];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
@@ -137,7 +137,7 @@ public class _5_11052_v2 {
 		String[] raws	 = br.readLine().split(" ");
 		
 		for( int i=1;i<=buyCardCount;i++ ){
-			cardPackPrice[i] = Integer.parseInt(raws[i-1]);
+			cardPackPrices[i] = Integer.parseInt(raws[i-1]);
 		}
 		
 		//System.out.println( TopDown(buyCardCount) );
@@ -159,7 +159,7 @@ public class _5_11052_v2 {
 		int max = 0;
 		
 		for( int i=1;i<=n;i++ ){
-			max = Math.max( max, TopDown(n-i)+cardPackPrice[i] );
+			max = Math.max( max, TopDown(n-i)+cardPackPrices[i] );
 		}
 		
 		memo[n] = max;
@@ -172,7 +172,7 @@ public class _5_11052_v2 {
 			for( int j=1;j<=i;j++ ){
 				// Math.max로 하나, 이거로 하나... 어차피 더 큰 값이 들어간다는 점이 같고 더 크지 않으면 그대로 그 값이 들어간다는건데 왜 다르지 결과가?
 				// 아 내가 카드팩 가격 배열에서 i번째의 값을 가져와서 값이 다른 것이었다..^^;
-                memo[i] = Math.max( memo[i], memo[i-j] + cardPackPrice[j] );
+                memo[i] = Math.max( memo[i], memo[i-j] + cardPackPrices[j] );
 			}
 		}
 		return memo[n];
